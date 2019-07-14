5768e5e1db974046bcacc4f55142d4be2460c32e
Paul Bradshaw <paulonhismobile@gmail.com>
2019-07-14 12:02:50 +0100

    book files

---
+++ r4journalistsbook/dylanscrapeprocess.R
@@ -0,0 +1,7 @@
+structure(list(type = c("Studio albums", "Live albums", "Compilation albums", 
+"Video albums", "Music videos", "EPs", "Singles", "Soundtrack albums", 
+"Tribute albums", "Box sets", "Bootleg Series"), number = c("38", 
+"13", "19", "11", "40", "26", "91", "7", "29", "13", "14"), numberclean = c(38, 
+13, 19, 11, 40, 26, 91, 7, 29, 13, 14), numberclean2 = c(38, 
+13, 19, 11, 40, 26, 91, 7, 29, 13, 14)), .Names = c("type", "number", 
+"numberclean", "numberclean2"), row.names = 3:13, class = "data.frame")
